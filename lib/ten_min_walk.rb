0212e5cc9a5ce5fa6dc80e5e9425cd695809d0d0
Brad Phillips <bjrp@protonmail.com>
2019-12-23 12:27:10 +0000

    New test passed

--- lib/ten_min_walk.rb
+++ lib/ten_min_walk.rb
@@ -1,3 +1,4 @@
 def ten_minutes_walk(directions)
-  directions.length == 10 ? true : false
+  # directions.length == 10 ? true : false
+  directions.count('w') == directions.count('e')
 end
--- spec/ten_min_walk_spec.rb
+++ spec/ten_min_walk_spec.rb
@@ -1,10 +1,20 @@
 require 'ten_min_walk'
 
 describe 'ten_minutes_walk' do
-  it "returns false if array's length is not 10" do
-    expect(ten_minutes_walk(['w', 's'])).to eq(false)
-  end
-  it "returns true if array's length is ten" do
-    expect(ten_minutes_walk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).to eq(true)
+  # context 'array length' do
+  #   it "returns false if array's length is not 10" do
+  #     expect(ten_minutes_walk(['w', 's'])).to eq(false)
+  #   end
+  #   it "returns true if array's length is ten" do
+  #     expect(ten_minutes_walk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).to eq(true)
+  #   end
+  # end
+  context 'return to start point' do
+    it 'returns false if directions do not return to start' do
+      expect(ten_minutes_walk(['w', 'n'])).to eq(false)
+    end
+    it 'returns true if directions return to start' do
+      expect(ten_minutes_walk(['w', 'e'])).to eq(true)
+    end
   end
 end
